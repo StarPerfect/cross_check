9bd25bb039d7099b007d5a45c468937d7ca5cd6a
Nancy Lee <srlee713@gmail.com>
2019-07-15 16:43:43 -0600

    Create 3 total_score methods and test them on dummy dataset

---
+++ lib/star_tracker.rb
@@ -0,0 +1,26 @@
+require 'csv'
+require 'pry'
+
+class StarTracker
+
+  def total_score
+    total_score = []
+    CSV.foreach('./test/dummy_data/dummy_game.csv', :headers => true, :col_sep => ',') do |row|
+      total_score << row[6].to_i + row[7].to_i
+    end
+    total_score
+  end
+
+  def highest_total_score
+    total_score.max
+  end
+
+  def lowest_total_score
+    total_score.min
+  end
+
+  def biggest_blowout
+    highest_total_score - lowest_total_score
+  end
+
+end
