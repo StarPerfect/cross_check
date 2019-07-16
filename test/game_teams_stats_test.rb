1b8ca753e4682ac448e33843c80cd451f9feaf01
Fenton Taylor <fentontaylor@gmail.com>
2019-07-16 13:33:48 -0600

    Rewrite GameTeamsStatsTest setup to read csv to make sure it behaves
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>, Corina Allen <47605558+StarPerfect@users.noreply.github.com>

--- test/game_teams_stats_test.rb
+++ test/game_teams_stats_test.rb
@@ -1,48 +1,38 @@
 require './test/test_helper'
 require 'Minitest/autorun'
 require 'Minitest/pride'
+require 'csv'
 require_relative '../lib/game_teams_stats'
 
 class GameTeamsStatsTest < Minitest::Test
   def setup
-    @stats = GameTeamsStats.new(
-      game_id: '2012030221',
-      team_id: '3',
-      home_or_away: 'away',
-      won: 'FALSE',
-      settled_in: 'OT',
-      head_coach: 'John Tortorella',
-      goals: '2',
-      shots: '35',
-      hits: '44',
-      pim: '8',
-      power_play_opportunities: '3',
-      power_play_goals: '0',
-      face_off_win_percentage: '44.8',
-      giveaways: '17',
-      takeaways: '7',
-      )
+    @game_teams_stats_data = CSV.read('./test/dummy_data/dummy_game_teams_stats.csv',
+      headers: true,
+      header_converters: :symbol
+    )
+    @stats = @game_teams_stats_data.map { |row| GameTeamsStats.new(row) }
+    @stats_1 = @stats[0]
   end
 
   def test_game_team_stats_exists
-    assert_instance_of GameTeamsStats, @stats
+    assert_instance_of GameTeamsStats, @stats_1
   end
 
   def test_attributes
-    assert_equal '2012030221', @stats.game_id
-    assert_equal '3', @stats.team_id
-    assert_equal 'away', @stats.home_or_away
-    assert_equal false, @stats.won
-    assert_equal 'OT', @stats.settled_in
-    assert_equal 'John Tortorella', @stats.head_coach
-    assert_equal 2, @stats.goals
-    assert_equal 35, @stats.shots
-    assert_equal 44, @stats.hits
-    assert_equal 8, @stats.pim
-    assert_equal 3, @stats.power_play_opportunities
-    assert_equal 0, @stats.power_play_goals
-    assert_equal 44.8, @stats.face_off_win_percentage
-    assert_equal 17, @stats.giveaways
-    assert_equal 7, @stats.takeaways
+    assert_equal '2012030221', @stats_1.game_id
+    assert_equal '3', @stats_1.team_id
+    assert_equal 'away', @stats_1.home_or_away
+    assert_equal false, @stats_1.won
+    assert_equal 'OT', @stats_1.settled_in
+    assert_equal 'John Tortorella', @stats_1.head_coach
+    assert_equal 2, @stats_1.goals
+    assert_equal 35, @stats_1.shots
+    assert_equal 44, @stats_1.hits
+    assert_equal 8, @stats_1.pim
+    assert_equal 3, @stats_1.power_play_opportunities
+    assert_equal 0, @stats_1.power_play_goals
+    assert_equal 44.8, @stats_1.face_off_win_percentage
+    assert_equal 17, @stats_1.giveaways
+    assert_equal 7, @stats_1.takeaways
   end
 end
