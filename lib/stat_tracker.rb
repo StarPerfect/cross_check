57fe94d48e7aef77dea4db81a2f2b126dc3992e2
Fenton Taylor <fentontaylor@gmail.com>
2019-07-16 13:33:48 -0600

    Rewrite #from_csv to create Game objects, add Game class.
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>, Corina Allen <47605558+StarPerfect@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,9 +1,21 @@
 require 'csv'
+require './lib/game'
 
 class StatTracker
-  def self.from_csv(files)
-    data = {}
-    files.each { |key, val| data[key] = CSV.table(val) }
-    data
+  attr_reader :games, :game_teams, :team_info
+
+  def initialize(args)
+    @games = args[:games]
+    @game_teams = args[:game_teams]
+    @team_info = args[:info]
+  end
+
+  def self.from_csv
+    games_data = CSV.read('./test/dummy_data/dummy_game.csv',
+      headers: true,
+      header_converters: :symbol)
+    games = games_data.map {|row| Game.new(row)}
+
+    StatTracker.new(games: games)
   end
 end
