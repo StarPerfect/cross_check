b850312f3ae49b4865b57ec2e9778984e3a147da
Nancy Lee <srlee713@gmail.com>
2019-07-16 13:45:58 -0600

    Create Team_Info class initialization method
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>, Corina Allen <47605558+StarPerfect@users.noreply.github.com>

---
+++ lib/team_info.rb
@@ -0,0 +1,19 @@
+class TeamInfo
+
+  attr_reader :team_id,
+              :franchiseId,
+              :shortName,
+              :teamName,
+              :abbreviation,
+              :link
+
+  def initialize(row)
+    @team_id = row[:team_id]
+    @franchiseId = row[:franchiseId]
+    @shortName = row[:shortName]
+    @teamName = row[:teamName]
+    @abbreviation = row[:abbreviation]
+    @link = row[:link]
+  end
+
+end
