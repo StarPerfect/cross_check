8864c3a15364ef605b05d80cae5cac4d1af04654
Nancy Lee <srlee713@gmail.com>
2019-07-16 14:43:34 -0600

    Add an argument to from_csv method

--- modules/total_score_test.rb
+++ modules/total_score_test.rb
@@ -6,10 +6,23 @@ require 'pry'
 
 class StatTrackerTest < Minitest::Test
 
+  def setup
+    game_path = './test/dummy_data/dummy_game.csv'
+    team_path = './test/dummy_data/dummy_team_info.csv'
+    game_teams_path = './test/dummy_data/dummy_game_teams_stats.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_1 = StatTracker.from_csv(locations)
+  end
+
   def test_dummy_total_score
-    stat_1 = StatTracker.from_csv
-    assert_equal 9, stat_1.highest_total_score
-    assert_equal 3, stat_1.lowest_total_score
-    assert_equal 6, stat_1.biggest_blowout
+    assert_equal 9, @stat_1.highest_total_score
+    assert_equal 3, @stat_1.lowest_total_score
+    assert_equal 6, @stat_1.biggest_blowout
   end
 end
