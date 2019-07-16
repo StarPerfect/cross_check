39820dd7d942e3a8c827888286f301da0d012e33
Corina Allen <StarPerfect@gmail.com>
2019-07-16 15:30:59 -0600

    Percentage_home_wins method complete

--- modules/float_mods.rb
+++ modules/float_mods.rb
@@ -1,12 +1,11 @@
 module FloatMods
   def percentage_home_wins
     home_wins = 0
-    @game_data.map do |game|
-      if game.include?('home')
+    @games.map do |game|
+      if game.outcome.include?('home')
         home_wins += 1
       end
     end
-      require "pry"; binding.pry
-    (home_wins / @game_data[:outcome].count).to_f * 100
+    ((home_wins.to_f / @games.count.to_f) * 100).round(2)
   end
 end
