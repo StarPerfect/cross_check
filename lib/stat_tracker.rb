9c8df5e439237e444e47432b23fed559633bfe7c
Nancy Lee <srlee713@gmail.com>
2019-07-16 14:30:10 -0600

    Add team_info code to from_csv class method
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>, Corina Allen <47605558+StarPerfect@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require './lib/game'
+require './lib/team_info'
 
 class StatTracker
   attr_reader :games, :game_teams, :team_info
@@ -16,6 +17,11 @@ class StatTracker
       header_converters: :symbol)
     games = games_data.map {|row| Game.new(row)}
 
-    StatTracker.new(games: games)
+    teams_data = CSV.read('./test/dummy_data/dummy_team_info.csv',
+      headers: true,
+      header_converters: :symbol)
+    team_info = teams_data.map {|row| TeamInfo.new(row)}
+
+    StatTracker.new(games: games, team_info: team_info)
   end
 end
