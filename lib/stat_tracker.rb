386a4fe5b907f0b8da6fbd96b02417e55fa340f8
Corina Allen <StarPerfect@gmail.com>
2019-07-16 16:01:44 -0600

    Move Test Helper file to very top of all tests to ensure a working Simplecov

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,9 +2,11 @@ require 'csv'
 require './lib/game'
 require './lib/team_info'
 require './lib/game_teams_stats'
-
+require './modules/float_mods'
 
 class StatTracker
+  include FloatMods
+
   attr_reader :games, :game_teams, :team_info
 
   def initialize(args)
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,3 +1,4 @@
+require './test/test_helper'
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/stat_tracker'
