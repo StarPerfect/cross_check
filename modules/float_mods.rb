c9c04d894a8bfdc19436a2993e8634bdf5fcdc8f
Corina Allen <StarPerfect@gmail.com>
2019-07-16 16:04:54 -0600

    Complete float mixins

--- modules/float_mods.rb
+++ modules/float_mods.rb
@@ -8,4 +8,15 @@ module FloatMods
     end
     ((home_wins.to_f / @games.count.to_f) * 100).round(2)
   end
+
+  def percentage_visitor_wins
+    visitor_wins = 0
+    @games.map do |game|
+      if game.outcome.include?('away')
+        visitor_wins += 1
+      end
+    end
+    ((visitor_wins.to_f / @games.count.to_f) * 100).round(2)
+
+  end
 end
