053511218bf5d78ca22fba6dc85417388157c6f9
Fenton Taylor <fentontaylor@gmail.com>
2019-07-16 10:35:51 -0600

    delete unecessary lines

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,18 +7,3 @@ class StatTracker
     data
   end
 end
-
-game_path = './test/dummy_data/dummy_game.csv'
-team_path = './test/dummy_data/dummy_team_info.csv'
-game_teams_path = './test/dummy_data/dummy_game_teams_stats.csv'
-
-files = {
-  games: game_path,
-  teams: team_path,
-  game_teams: game_teams_path
-}
-
-stats_1 = StatTracker.from_csv(files)
-stats_2 = StatTracker.from_csv_2(files)
-
-require 'pry';binding.pry
