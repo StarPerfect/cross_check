2a07edff41cb01e79ff50cdf86d7b36865495397
Fenton Taylor <fentontaylor@gmail.com>
2019-07-17 11:31:01 -0600

    Create #team_total_games helper method

--- modules/best_worst_o_and_d.rb
+++ modules/best_worst_o_and_d.rb
@@ -1,3 +1,23 @@
 module BestWorstOAndD
+  def method_name
+    team_goals = Hash.new(0)
+    team_total_games = Hash.new(0)
 
+    @game_teams.each do |team|
+      team_goals[team.team_id] += team.goals
+      team_total_games[team.team_id] += 1
+    end
+
+    team_goals.each do |team, goals|
+      team_goals[team] = (goals.to_f / team_total_games[team])
+    end
+
+
+  end
+
+  def team_total_games
+    each_total_games = Hash.new(0)
+    @game_teams.each { |team| each_total_games[team.team_id] += 1}
+    each_total_games
+  end
 end
