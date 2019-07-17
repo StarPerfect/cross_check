5d98e7c7fc4c346b5ff2291fca36c2d9fc74980b
Fenton Taylor <fentontaylor@gmail.com>
2019-07-17 11:45:45 -0600

    Finish #best_offense, make snake_case variables/readers for team_info

--- modules/best_worst_o_and_d.rb
+++ modules/best_worst_o_and_d.rb
@@ -1,18 +1,17 @@
 module BestWorstOAndD
-  def method_name
+  def best_offense
     team_goals = Hash.new(0)
-    team_total_games = Hash.new(0)
 
     @game_teams.each do |team|
       team_goals[team.team_id] += team.goals
-      team_total_games[team.team_id] += 1
     end
 
     team_goals.each do |team, goals|
       team_goals[team] = (goals.to_f / team_total_games[team])
     end
 
-
+    best_o = team_goals.max_by {|team, goal_avg| goal_avg}.first
+    @team_info.find { |team| team.team_id == best_o}.team_name
   end
 
   def team_total_games
