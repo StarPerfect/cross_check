0224fc2750930eb6bec0afbc53f7573c1c17cc8a
Corina Allen <StarPerfect@gmail.com>
2019-07-18 14:07:15 -0600

    Most goals scored and fewest goals scored methods complete

--- modules/corina_it4.rb
+++ modules/corina_it4.rb
@@ -7,4 +7,26 @@ module CorinaIt4
     id = get_team_id_from_name(team)
     (wins_per_team[id].to_f / team_total_games[id] * 100).round(2)
   end
+
+  def most_goals_scored(team)
+    id = get_team_id_from_name(team)
+    max_score = 0
+    @game_teams.each do |team|
+      if team.team_id == id && team.goals > max_score
+        max_score = team.goals
+      end
+    end
+    max_score
+  end
+
+  def fewest_goals_scored(team)
+    id = get_team_id_from_name(team)
+    min_score = 100
+    @game_teams.each do |team|
+      if team.team_id == id && team.goals < min_score
+        min_score = team.goals
+      end
+    end
+    min_score
+  end
 end
--- test/corina_it4_test.rb
+++ test/corina_it4_test.rb
@@ -13,4 +13,12 @@ class CorinaIt4Test < Minitest::Test
   def test_average_win_percentage
     assert_equal 87.5, @stat_tracker.average_win_percentage('Bruins')
   end
+
+  def test_most_goals_scored
+    assert_equal 6, @stat_tracker.most_goals_scored('Bruins')
+  end
+
+  def test_fewest_goals_scored
+    assert_equal 2, @stat_tracker.fewest_goals_scored('Bruins')
+  end
 end
