6662b02585bfa91bb9a3db1c504d00242a857d11
Corina Allen <StarPerfect@gmail.com>
2019-07-19 13:44:17 -0600

    Most Hits working

--- modules/corina_it5.rb
+++ modules/corina_it5.rb
@@ -1,14 +1,14 @@
 module CorinaIt5
-  def most_hits
-    num_of_hits = 0
-    id = 0
-    @game_teams.each do |team|
-      require 'pry'; binding.pry
-      if team.hits > num_of_hits
-        num_of_hits = team.hits
-        id = team.team_id
-      end
+  def most_hits(season)
+    game_ids = @games.find_all { |game| game.season == season }.map{|game_id| game_id.game_id}
+    game_teams_in_season = @game_teams.find_all do |game_team|
+      game_ids.include? game_team.game_id
     end
-    get_team_name(id)
+    hits_hash = Hash.new(0)
+    game_teams_in_season.each do |game_team|
+      hits_hash[game_team.team_id] += game_team.hits
+    end
+    most_hits_team = hits_hash.max_by{|key,value| value}.first
+    get_team_name(most_hits_team)
   end
 end
