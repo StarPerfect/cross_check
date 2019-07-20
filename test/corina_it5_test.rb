0550a6ada965d456233899d26822324447e1adf7
Corina Allen <StarPerfect@gmail.com>
2019-07-19 18:28:56 -0600

    Complete Power Play Goal Percentage

--- test/corina_it5_test.rb
+++ test/corina_it5_test.rb
@@ -11,10 +11,14 @@ class CorinaIt5Test < Minitest::Test
   end
 
   def test_most_hits
-    assert_equal 'Rangers', @stat_tracker.most_hits("20122013")
+    assert_equal 'Bruins', @stat_tracker.most_hits("20122013")
   end
 
   def test_fewest_hits
-    assert_equal 'Bruins', @stat_tracker.fewest_hits("20122013")
+    assert_equal 'Penguins', @stat_tracker.fewest_hits("20122013")
+  end
+
+  def test_power_play_goal_percentage
+    assert_equal 0.15, @stat_tracker.power_play_goal_percentage("20132013")
   end
 end
