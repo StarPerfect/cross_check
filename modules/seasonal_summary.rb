0c9eefa0667b8e2007760731e47a4238326f41b0
Fenton Taylor <fentontaylor@gmail.com>
2019-07-21 09:04:32 -0600

    seasonal_summary working!
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>
    Co-authored-by: Corina Allen <47605558+StarPerfect@users.noreply.github.com>

--- modules/seasonal_summary.rb
+++ modules/seasonal_summary.rb
@@ -1,42 +1,71 @@
 module SeasonalSummary
-  def seasonal_summary(team_id)
-    all_team_games = @games.find_all{ |game| (game.away_team_id == team_id || game.home_team_id == team_id) }
+  def seasonal_summary(id)
+    all_team_games = @games.find_all{ |game| (game.away_team_id == id || game.home_team_id == id) }
     games_by_season = all_team_games.group_by do |game|
       game.season
     end
     games_by_season.transform_values! do |games|
       games.group_by{ |game| game.type }
     end
-    w = games_by_season.each{ |season, p_or_r| games_by_season[season] = {
+    games_by_season.each{ |season, p_or_r| games_by_season[season] = {
       postseason: {
-        win_percentage: win_percent(games_by_season[season]['P']),
-        total_goals_scored: tot_goals(games_by_season[season]['P']),
-        total_goals_against: against_goals(games_by_season[season]['P']),
-        average_goals_scored: avg_goals(games_by_season[season]['P']),
-        average_goals_against: against_avg(games_by_season[season]['P'])
+        win_percentage: win_percent(games_by_season[season]['P'], id),
+        total_goals_scored: tot_goals(games_by_season[season]['P'], id),
+        total_goals_against: against_goals(games_by_season[season]['P'], id),
+        average_goals_scored: avg_goals(games_by_season[season]['P'], id),
+        average_goals_against: against_avg(games_by_season[season]['P'], id)
     },
       regular_season: {
-        win_percentage: win_percent(games_by_season[season]['R']),
-        total_goals_scored: tot_goals(games_by_season[season]['R']),
-        total_goals_against: against_goals(games_by_season[season]['R']),
-        average_goals_scored: avg_goals(games_by_season[season]['R']),
-        average_goals_against: against_avg(games_by_season[season]['R'])
-        }}}
-require 'pry'; binding.pry
+        win_percentage: win_percent(games_by_season[season]['R'], id),
+        total_goals_scored: tot_goals(games_by_season[season]['R'], id),
+        total_goals_against: against_goals(games_by_season[season]['R'], id),
+        average_goals_scored: avg_goals(games_by_season[season]['R'], id),
+        average_goals_against: against_avg(games_by_season[season]['R'], id)
+        }
+      }
+    }
+
   end
 
-  def win_percent(games)
+  def win_percent(games, id)
+    wins = games.count do |game|
+      if game.away_team_id == id
+        game.away_goals > game.home_goals
+      else
+        game.home_goals > game.away_goals
+      end
+    end
+    ( wins.to_f / games.length )
+    .round(2)
   end
 
-  def tot_goals(games)
+  def tot_goals(games, id)
+    games.sum do |game|
+      if game.away_team_id == id
+        game.away_goals
+      else
+        game.home_goals
+      end
+    end
   end
 
-  def against_goals(games)
+  def against_goals(games, id)
+    games.sum do |game|
+      if game.away_team_id == id
+        game.home_goals
+      else
+        game.away_goals
+      end
+    end
   end
 
-  def avg_goals(games)
+  def avg_goals(games, id)
+    ( tot_goals(games, id).to_f / games.length )
+    .round(2)
   end
 
-  def against_avg(games)
+  def against_avg(games, id)
+  ( against_goals(games, id).to_f / games.length )
+  .round(2)
   end
 end
