d10b14e3af1bb2e39398f5ac3536d4ffe6a918b1
Nancy Lee <srlee713@gmail.com>
2019-07-24 16:31:34 -0600

    Add HTML and styling
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Corina Allen <47605558+StarPerfect@users.noreply.github.com>

--- page_generator.rb
+++ page_generator.rb
@@ -18,5 +18,15 @@ locations = {
 @stat_tracker = StatTracker.from_csv(locations)
 
 template = File.read('./site/index.html.erb')
+game = File.read('./site/stats/game.html.erb')
+league = File.read('./site/stats/league.html.erb')
+team = File.read('./site/stats/team.html.erb')
+season = File.read('./site/stats/season.html.erb')
+season_sum = File.read('./site/stats/season_sum.html.erb')
 
 File.write('./site/index.html', ERB.new(template).result(binding))
+File.write('./site/stats/game.html', ERB.new(game).result(binding))
+File.write('./site/stats/league.html', ERB.new(league).result(binding))
+File.write('./site/stats/team.html', ERB.new(team).result(binding))
+File.write('./site/stats/season.html', ERB.new(season).result(binding))
+File.write('./site/stats/season_sum.html', ERB.new(season_sum).result(binding))
